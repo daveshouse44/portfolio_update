1f999cea84fdab30ba1e382eb909b0c6954738c2
David Tierney <daveshouse44@hotmail.com>
2022-06-05 23:56:36 -0500

    added images

---
+++ src/images/images.js
@@ -0,0 +1,46 @@
+import about01 from "../assets/about01.png";
+import about02 from "../assets/about02.png";
+import about03 from "../assets/about03.png";
+import about04 from "../assets/about04.png";
+
+import api from "../assets/api.png";
+import css from "../assets/css.png";
+import git from "../assets/git.png";
+import graphql from "../assets/graphql.png";
+import html from "../assets/html.png";
+import javascript from "../assets/javascript.png";
+import node from "../assets/node.png";
+import python from "../assets/python.png";
+import react from "../assets/react.png";
+import sass from "../assets/sass.png";
+import vue from "../assets/vue.png";
+
+import brand from "../assets/brand.png";
+import brick from "../assets/brick.png";
+import circle from "../assets/circle.svg";
+import profile from "../assets/profile.png";
+
+import mobile from "../assets/mobile.png";
+
+export default {
+  about01,
+  about02,
+  about03,
+  about04,
+  api,
+  css,
+  git,
+  graphql,
+  html,
+  javascript,
+  node,
+  python,
+  react,
+  sass,
+  vue,
+  brand,
+  brick,
+  circle,
+  profile,
+  mobile,
+};
