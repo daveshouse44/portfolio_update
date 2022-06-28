6b6fa80f04554e2a08d02908dbf9148ebd051de9
David Tierney <daveshouse44@hotmail.com>
2022-06-28 00:29:57 -0500

    added footer on small screen

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,65 @@
 import React from "react";
-
+import { BsGithub, BsLinkedin, BsStackOverflow } from "react-icons/bs";
+import { FaRedditAlien } from "react-icons/fa";
+import { SiDiscord } from "react-icons/si";
+import { GrMail } from "react-icons/gr";
 import "./Footer.scss";
 
 const Footer = () => {
-  return <div>Set here</div>;
+  return (
+    <div className="app__footer">
+      <div>
+        <a
+          href="https://github.com/daveshouse44"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <BsGithub />
+        </a>
+      </div>
+      <div>
+        <a
+          href="https://www.linkedin.com/in/david-tierney-652030214/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <BsLinkedin />
+        </a>
+      </div>
+      <div>
+        <a
+          href="https://stackoverflow.com/users/16111199/david-tierney"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <BsStackOverflow />
+        </a>
+      </div>
+      <div>
+        <a
+          href="https://discordapp.com/users/daveshouse44#3785"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <SiDiscord />
+        </a>
+      </div>
+      <div>
+        <a
+          href="https://www.reddit.com/user/daves_house"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaRedditAlien />
+        </a>
+      </div>
+      <div>
+        <a href="mailto:me@davetierney.dev">
+          <GrMail />
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
