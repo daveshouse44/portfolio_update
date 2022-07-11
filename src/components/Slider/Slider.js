3f62f08009f6ebc0662154295a6e6b0b2a3e788e
David Tierney <daveshouse44@hotmail.com>
2022-07-10 23:39:06 -0500

    added skills to about page

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,74 +1,55 @@
 import React from "react";
-import { animated, useSpring } from "react-spring";
-// import { useGesture } from "react-use-gesture";
 import { images } from "../../images";
+import { MdChevronLeft, MdChevronRight } from "react-icons/md";
 import "./Slider.scss";
 
-// const clamp = (value, clampAt = 30) => {
-//   if (value > 0) {
-//     return value > clampAt ? clampAt : value;
-//   } else {
-//     return value < -clampAt ? -clampAt : value;
-//   }
-// };
-
 const Slider = () => {
-  const [style, set] = useSpring(() => ({
-    transform: "perspective(500px) rotateY(0deg)",
-  }));
+  const slideLeft = () => {
+    var slider = document.getElementById("slider__element");
+    slider.scrollLeft = slider.scrollLeft - 500;
+  };
 
-  //   const bind = useGesture({
-  //     onScroll: (event) => {
-  //       set({
-  //         transform: `perspective(500px) rotateY(${
-  //           event.scrolling ? clamp(event.delta[0]) : 0
-  //         }deg)`,
-  //       });
-  //     },
-  //     onDrag: (event) => {
-  //       if (event.dragging) {
-  //         document.querySelector(".container").scrollLeft -= event.delta[0];
-  //       }
-  //     },
-  //     onWheel: (event) => {
-  //       if (event.wheeling) {
-  //         document.querySelector(".container").scrollLeft += event.delta[1];
-  //       }
-  //     },
-  //   });
-
+  const slideRight = () => {
+    var slider = document.getElementById("slider__element");
+    slider.scrollLeft = slider.scrollLeft + 500;
+  };
   return (
     <>
       <div className="slider__container">
-        {[
-          images.html,
-          images.css,
-          images.javascript,
-          images.tailwind,
-          images.sass,
-          images.bootstrap,
-          images.node,
-          images.mongodb,
-          images.react,
-          images.vue,
-          images.angular,
-          images.graphql,
-          images.python,
-          images.heroku,
-          images.mysql,
-          images.aws,
-          images.git,
-          images.wordpress,
-        ].map((src) => (
-          <animated.div
-            key={src}
-            className="slider__card"
-            style={{
-              ...style,
-              backgroundImage: `url(${src})`,
-            }}
-          />
-        ))}
+        <MdChevronLeft
+          className="slider__chevron"
+          onClick={slideLeft}
+          size={45}
+        />
+        <div id="slider__element" className="slider__card">
+          {[
+            images.html,
+            images.css,
+            images.javascript,
+            images.tailwind,
+            images.sass,
+            images.bootstrap,
+            images.node,
+            images.mongodb,
+            images.react,
+            images.vue,
+            images.angular,
+            images.graphql,
+            images.python,
+            images.heroku,
+            images.mysql,
+            images.aws,
+            images.git,
+            images.wordpress,
+          ].map((src) => (
+            <img className="slider__image" key={src} src={src} alt="/" />
+          ))}
+        </div>
+        <MdChevronRight
+          className="slider__chevron"
+          onClick={slideRight}
+          size={45}
+        />
       </div>
     </>
   );
